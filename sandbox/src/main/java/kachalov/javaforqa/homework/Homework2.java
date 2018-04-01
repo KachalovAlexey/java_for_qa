ac23acd0f914acd00491ebc369ad5814024fcef9
KachalovAlexey <kachalov123@yandex.ru>
2018-04-01 23:14:40 +0300

    Задание №2, пункт 4

--- sandbox/src/main/java/kachalov/javaforqa/homework/Homework2.java
+++ sandbox/src/main/java/kachalov/javaforqa/homework/Homework2.java
@@ -4,29 +4,12 @@ public class Homework2 {
 
     public static void main(String[] args) {
 
-        Point p1 = new Point();
-        p1.x = 3;
-        p1.y = 4;
-        Point p2 = new Point();
-        p2.x = 5;
-        p2.y = 6;
+        Point l1 = new Point(3, 4, 5,6);
+        System.out.println("расстояние между точками 3.4 и 5.6 = " + l1.distance());
 
-        System.out.println("расстояние между точками 3.4 и 5.6 = " + distance(p1,p2));
+        Point l2 = new Point(7, 1, 8,2);
+        System.out.println("расстояние между точками 7.1 и 8.2 = " + l2.distance());
 
-        Point p3 = new Point();
-        p3.x = 7;
-        p3.y = 1;
-        Point p4 = new Point();
-        p4.x = 8;
-        p4.y = 2;
-
-        System.out.println("расстояние между точками 7.1 и 8.2 = " + distance(p3,p4));
-    }
-
-    public static double distance(Point p1, Point p2){
-        double a = Math.pow((p2.x - p1.x), 2);
-        double b = Math.pow((p2.y - p1.y), 2);
-        return Math.sqrt(a + b);
     }
 
 }
--- sandbox/src/main/java/kachalov/javaforqa/homework/Point.java
+++ sandbox/src/main/java/kachalov/javaforqa/homework/Point.java
@@ -2,7 +2,22 @@ package kachalov.javaforqa.homework;
 
 public class Point {
 
-    public double x;
-    public double y;
+    public double x1;
+    public double y1;
+    public double x2;
+    public double y2;
+
+    public Point (double x1, double y1, double x2, double y2){
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+    }
+
+    public double distance(){
+        double a = Math.pow((x2 - x1), 2);
+        double b = Math.pow((y2 - y1), 2);
+        return Math.sqrt(a + b);
+    }
 }
 
