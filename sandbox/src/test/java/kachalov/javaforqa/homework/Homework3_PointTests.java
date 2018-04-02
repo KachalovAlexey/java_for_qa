7408ab385f747c58e7393ee2cd994c5bcabdde6e
KachalovAlexey <kachalov123@yandex.ru>
2018-04-02 21:16:29 +0300

    Задание №3, вторая попытка

--- sandbox/src/test/java/kachalov/javaforqa/homework/Homework3_PointTests.java
+++ sandbox/src/test/java/kachalov/javaforqa/homework/Homework3_PointTests.java
@@ -7,20 +7,23 @@ public class Homework3_PointTests {
 
     @Test
     public void testDistance1(){
-        Point l = new Point(3,4,5,6);
-        Assert.assertEquals(l.distance(), 2.8284271247461903);
+        Point p1 = new Point(3,4);
+        Point p2 = new Point(5,6);
+        Assert.assertEquals(p1.distance(p2), 2.8284271247461903);
     }
 
     @Test
     public void testDistance2(){
-        Point l = new Point(7,1,8,2);
-        Assert.assertEquals(l.distance(), 1.4142135623730951);
+        Point p1 = new Point(7,1);
+        Point p2 = new Point(8,2);
+        Assert.assertEquals(p1.distance(p2), 1.4142135623730951);
     }
 
     @Test
     public void testDistance3(){
-        Point l = new Point(12,23,1,1);
-        Assert.assertEquals(l.distance(), 24.596747752497688);
+        Point p1 = new Point(12,23);
+        Point p2 = new Point(1,1);
+        Assert.assertEquals(p1.distance(p2), 24.596747752497688);
     }
 
 }
