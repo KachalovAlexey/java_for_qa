08de6dc1c56a20366d63e9858e26b3e395ab40b3
KachalovAlexey <kachalov123@yandex.ru>
2018-04-02 21:11:40 +0300

    Задание №2, вторая попытка

--- sandbox/src/main/java/kachalov/javaforqa/homework/Homework2.java
+++ sandbox/src/main/java/kachalov/javaforqa/homework/Homework2.java
@@ -4,11 +4,10 @@ public class Homework2 {
 
     public static void main(String[] args) {
 
-        Point l1 = new Point(3, 4, 5,6);
-        System.out.println("расстояние между точками 3.4 и 5.6 = " + l1.distance());
+        Point p1 = new Point(3, 4);
+        Point p2 = new Point(5, 6);
 
-        Point l2 = new Point(7, 1, 8,2);
-        System.out.println("расстояние между точками 7.1 и 8.2 = " + l2.distance());
+        System.out.println("Расстояние между точками = " + p1.distance(p2));
 
     }
 
