b1e6cc35f24cd3057760f44a77d147ca75da3e6c
KachalovAlexey <kachalov123@yandex.ru>
2018-04-24 21:21:57 +0300

    Лекция 4.7

--- addressbook-web-tests/src/test/java/kachalov/javaforqa/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/kachalov/javaforqa/addressbook/tests/GroupDeletionTests.java
@@ -24,7 +24,6 @@ public class GroupDeletionTests extends TestBase {
 
         before.remove(before.size() - 1);
         Assert.assertEquals(before, after);
-
     }
 
 }
--- addressbook-web-tests/src/test/java/kachalov/javaforqa/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/kachalov/javaforqa/addressbook/tests/GroupModificationTests.java
@@ -4,6 +4,7 @@ import kachalov.javaforqa.addressbook.model.GroupData;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupModificationTests extends TestBase{
@@ -17,11 +18,16 @@ public class GroupModificationTests extends TestBase{
         List<GroupData> before = app.getGroupHelper().getGroupList();
         app.getGroupHelper().selectGroup(before.size() - 1);
         app.getGroupHelper().initGroupModification();
-        app.getGroupHelper().fillGroupForm(new GroupData("test1", "test3", "test4"));
+        GroupData group = new GroupData(before.get(before.size() - 1).getId(), "test1", "test3", "test4");
+        app.getGroupHelper().fillGroupForm(group);
         app.getGroupHelper().submitGroupModification();
         app.getNavigationManager().gotoGroupPage();
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size());
+
+        before.remove(before.size() -1);
+        before.add(group);
+        Assert.assertEquals(new HashSet<Object>(after), new HashSet<Object>(before));
     }
 
 }
