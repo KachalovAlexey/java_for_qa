6af0674adb5529c3dc63b9aeb967c12d4b278306
KachalovAlexey <kachalov123@yandex.ru>
2018-05-02 15:17:23 +0300

    Лекция 5.5

--- addressbook-web-tests/src/test/java/kachalov/javaforqa/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/kachalov/javaforqa/addressbook/appmanager/GroupHelper.java
@@ -4,9 +4,9 @@ import kachalov.javaforqa.addressbook.model.GroupData;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-
-import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class GroupHelper extends HelperBase {
 
@@ -36,8 +36,8 @@ public class GroupHelper extends HelperBase {
         click(By.xpath("//div[@id='content']/form/input[5]"));
     }
 
-    public void selectGroup(int index) {
-        wd.findElements(By.name("selected[]")).get(index).click();
+    public void selectGroupByID(int id) {
+        wd.findElement(By.cssSelector("input[value='" + id +"']")).click();
     }
 
     public void initGroupModification() {
@@ -55,16 +55,16 @@ public class GroupHelper extends HelperBase {
         returnToGroupPage();
     }
 
-    public void modify(int index, GroupData group) {
-        selectGroup(index);
+    public void modify(GroupData group) {
+        selectGroupByID(group.getId());
         initGroupModification();
         fillGroupForm(group);
         submitGroupModification();
         returnToGroupPage();
     }
 
-    public void delete(int index) {
-        selectGroup(index);
+    public void delete(GroupData group) {
+        selectGroupByID(group.getId());
         deleteSelectedGroups();
         returnToGroupPage();
     }
@@ -78,8 +78,8 @@ public class GroupHelper extends HelperBase {
        return wd.findElements(By.name("selected[]")).size();
     }
 
-    public List<GroupData> list() {
-        List<GroupData> groups = new ArrayList<>();
+    public Set<GroupData> all() {
+        Set<GroupData> groups = new HashSet<GroupData>();
         List<WebElement> elements = wd.findElements(By.cssSelector("span.group"));
         for (WebElement element : elements) {
             String name = element.getText();
@@ -88,4 +88,6 @@ public class GroupHelper extends HelperBase {
         }
         return groups;
     }
+
+
 }
--- addressbook-web-tests/src/test/java/kachalov/javaforqa/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/kachalov/javaforqa/addressbook/tests/GroupDeletionTests.java
@@ -4,16 +4,14 @@ import kachalov.javaforqa.addressbook.model.GroupData;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
-import java.util.Comparator;
-import java.util.List;
+import java.util.Set;
 
 public class GroupDeletionTests extends TestBase {
 
     @BeforeMethod
     public void ensurePreconditions(){
         app.goTo().groupPage();
-        if (app.group().list().size() == 0){
+        if (app.group().all().size() == 0){
             app.group().create(new GroupData().withName("test1"));
         }
     }
@@ -22,16 +20,13 @@ public class GroupDeletionTests extends TestBase {
     @Test
     public void testGroupDeletion() {
         app.goTo().groupPage();
-        List<GroupData> before = app.group().list();
-        int index = before.size() - 1;
-        app.group().delete(index);
-        List<GroupData> after = app.group().list();
-        Assert.assertEquals(after.size(), index);
+        Set<GroupData> before = app.group().all();
+        GroupData deletedGroup = before.iterator().next();
+        app.group().delete(deletedGroup);
+        Set<GroupData> after = app.group().all();
+        Assert.assertEquals(after.size(), before.size() - 1);
 
-        before.remove(index);
-        Comparator<? super GroupData> byID = Comparator.comparingInt(GroupData::getId);
-        before.sort(byID);
-        after.sort(byID);
+        before.remove(deletedGroup);
         Assert.assertEquals(after, before);
     }
 
