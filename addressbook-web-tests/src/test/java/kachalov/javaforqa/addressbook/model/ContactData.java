1b930c9d25e95446331a20d38934f070063fe2fe
KachalovAlexey <kachalov123@yandex.ru>
2018-05-02 13:31:11 +0300

    убра сранение по ID для контактов

--- addressbook-web-tests/src/test/java/kachalov/javaforqa/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/kachalov/javaforqa/addressbook/model/ContactData.java
@@ -26,7 +26,6 @@ public class ContactData {
         this.id = id;
         this.firstname = firstname;
         this.lastname = lastname;
-
         this.mobile = mobile;
         this.email = email;
         this.group = group;
@@ -68,19 +67,22 @@ public class ContactData {
                 ", lastname='" + lastname + '\'' +
                 '}';
     }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
+
         ContactData that = (ContactData) o;
-        return id == that.id &&
-                Objects.equals(firstname, that.firstname) &&
-                Objects.equals(lastname, that.lastname);
+
+        if (firstname != null ? !firstname.equals(that.firstname) : that.firstname != null) return false;
+        return lastname != null ? lastname.equals(that.lastname) : that.lastname == null;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, firstname, lastname);
+        int result = firstname != null ? firstname.hashCode() : 0;
+        result = 31 * result + (lastname != null ? lastname.hashCode() : 0);
+        return result;
     }
-
 }
