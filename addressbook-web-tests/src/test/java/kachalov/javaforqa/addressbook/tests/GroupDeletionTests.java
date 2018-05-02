fa5fac66674a2cbe13df87e1e43e42d06b63022e
KachalovAlexey <kachalov123@yandex.ru>
2018-05-02 13:54:49 +0300

    Лекция 5.2

--- addressbook-web-tests/src/test/java/kachalov/javaforqa/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/kachalov/javaforqa/addressbook/tests/GroupDeletionTests.java
@@ -2,6 +2,7 @@ package kachalov.javaforqa.addressbook.tests;
 
 import kachalov.javaforqa.addressbook.model.GroupData;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.Comparator;
@@ -9,16 +10,19 @@ import java.util.List;
 
 public class GroupDeletionTests extends TestBase {
 
+    @BeforeMethod
+    public void ensurePreconditions(){
+        app.getNavigationManager().gotoGroupPage();
+        if (! app.getGroupHelper().isThereAGroup()){
+            app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+        }
+    }
+
     
     @Test
     public void testGroupDeletion() {
         app.getNavigationManager().gotoGroupPage();
         List<GroupData> before = app.getGroupHelper().getGroupList();
-        if (! app.getGroupHelper().isThereAGroup()){
-            GroupData group = new GroupData("test1", null, null);
-            app.getGroupHelper().createGroup(group);
-            before.add(group);
-        }
         app.getGroupHelper().selectGroup(before.size() - 1);
         app.getGroupHelper().deleteSelectedGroups();
         app.getGroupHelper().returnToGroupPage();
