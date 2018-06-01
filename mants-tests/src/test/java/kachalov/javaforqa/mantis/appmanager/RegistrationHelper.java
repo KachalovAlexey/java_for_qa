930b7d5a24440ef96f3f2c799752ae851285cf9e
KachalovAlexey <kachalov123@yandex.ru>
2018-06-01 14:33:35 +0300

    Лекция 8.6

--- mants-tests/src/test/java/kachalov/javaforqa/mantis/appmanager/RegistrationHelper.java
+++ mants-tests/src/test/java/kachalov/javaforqa/mantis/appmanager/RegistrationHelper.java
@@ -1,17 +1,24 @@
 package kachalov.javaforqa.mantis.appmanager;
 
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.By;
 
-public class RegistrationHelper {
-    private final ApplicationManager app;
-    private WebDriver wd;
+public class RegistrationHelper extends HelperBase{
 
     public RegistrationHelper(ApplicationManager app) {
-        this.app = app;
-        wd = app.getDriver();
+        super(app);
     }
 
-    public void start(String user1, String email) {
+    public void start(String username, String email) {
         wd.get(app.getProperty("web.BaseUrl") + "/signup_page.php");
+        type(By.name("username"), username);
+        type(By.name("email"), email);
+        click(By.cssSelector("[type='submit']"));
+    }
+
+    public void finish(String confirmatonLink, String password) {
+        wd.get(confirmatonLink);
+        type(By.name("password"), password);
+        type(By.name("password_confirm"), password);
+        click(By.cssSelector("input[value='Update User']"));
     }
 }
--- mants-tests/src/test/java/kachalov/javaforqa/mantis/tests/RegistrationTests.java
+++ mants-tests/src/test/java/kachalov/javaforqa/mantis/tests/RegistrationTests.java
@@ -1,11 +1,42 @@
 package kachalov.javaforqa.mantis.tests;
 
+import kachalov.javaforqa.mantis.model.MailMessage;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import ru.lanwen.verbalregex.VerbalExpression;
+import java.util.List;
 
 public class RegistrationTests extends TestBase{
 
-    @Test
-    public void testRegistration() {
-        app.registration().start("user1", "user1@localhost.localadmin");
+    @BeforeMethod
+    public void startMailServer() {
+        app.mail().start();
     }
+
+    @Test
+    public void testRegistration() throws Exception {
+        long now = System.currentTimeMillis();
+        String email = "user" + now + "@localhost.localadmin";
+        String user = "user" + now;
+        String password = "password";
+        app.registration().start(user, email);
+        List<MailMessage> mailMessages = app.mail().waitForMail(2, 10000);
+        String confirmatonLink = findConfirmationLink(mailMessages, email);
+        app.registration().finish(confirmatonLink, password);
+        Assert.assertTrue(app.newSession().login(user, password));
+    }
+
+    private String findConfirmationLink(List<MailMessage> mailMessages, String email) {
+        MailMessage mailMessage = mailMessages.stream().filter(m -> m.to.equals(email)).findFirst().get();
+        VerbalExpression regex = VerbalExpression.regex().find("http://").nonSpace().oneOrMore().build();
+        return regex.getText(mailMessage.text);
+    }
+
+    @AfterMethod (alwaysRun = true)
+    public void stopMailServer() {
+        app.mail().stop();
+    }
+
 }
